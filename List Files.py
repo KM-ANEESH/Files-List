b6dc1af321ccb8edc10825fd6fc865a7f871b37a
KM-ANEESH <78631423+KM-ANEESH@users.noreply.github.com>
2023-03-13 19:52:47 +0530

    Add files via upload

--- List Files.py
+++ List Files.py
@@ -4,6 +4,6 @@ list_files = os.listdir(from_dir)
 print (list_files)
 for file_name in list_files:
     print(file_name)
-    root,exe =  os.path.splitext("Desktop/")
+    root,exe =  os.path.splitext(file_name)
     print(root)
     print(exe)
